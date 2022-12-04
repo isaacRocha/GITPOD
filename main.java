33824e22e9be1fb962c7a994b8e39f87f61d80ae
isaacRocha <isaacrc74@gmail.com>
2022-12-04 23:49:36 +0000

    teste do gitpod

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class main{
+    public static void main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine(); 
+        System.out.println("Ola: "+ nome);
+
+        
+    }
+}
